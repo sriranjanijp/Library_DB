bcb7e60e964cd3bf7c84116c76625f58d003e83c
sriranjanijp <sriranjani.jp@gmail.com>
2025-03-05 18:56:00 +0530

    Set up endpoints

--- library_backend/library/views.py
+++ library_backend/library/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
